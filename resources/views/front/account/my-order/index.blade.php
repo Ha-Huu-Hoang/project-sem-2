024014f41891158040c54dce0771fdffefb583f3
NgoManhSon <ngomanhson2004txpt@gmail.com>
2023-06-25 01:48:24 +0700

    Add payment method, status

--- resources/views/front/account/my-order/index.blade.php
+++ resources/views/front/account/my-order/index.blade.php
@@ -23,7 +23,6 @@
         <div class="container">
             <div class="order-table" style="margin: 50px 0">
                 @if(count($orders) > 0)
-                    @foreach($orders as $order)
                     <table class="table table-borderless">
                         <thead class="table-bordered">
                             <tr>
@@ -31,44 +30,58 @@
                                 <th scope="col">ID</th>
                                 <th scope="col">Product</th>
                                 <th scope="col">Total</th>
+                                <th scope="col">Payment Method</th>
+                                <th scope="col">Status</th>
                                 <th scope="col">Details</th>
                             </tr>
                         </thead>
                         <tbody class="table-bordered">
-                            <tr>
-                            <td style="width: 18%">
-                                @if(isset($order->orderDetails) && count($order->orderDetails) > 0)
-                                    @foreach($order->orderDetails as $orderDetail)
-                                        @if(isset($orderDetail->product) && isset($orderDetail->product->productImages) && count($orderDetail->product->productImages) > 0)
-                                            <img src="{{$orderDetail->product->productImages[0]->path}}" alt="..." class="img-thumbnail" style="width: 100px; height: 100px; object-fit: cover">
-                                            @break
-                                        @endif
-                                    @endforeach
-                                @endif
-                            </td>
-                            <td>#{{$order->id}}</td>
-                            <td>
-                                @if(isset($order->orderDetails) && count($order->orderDetails) > 0)
-                                    {{$order->orderDetails[0]->product->name}}
-
-                                    @php
-                                        $totalQuantity = $order->orderDetails->sum('qty');
-                                        $otherProductsCount = $totalQuantity - 1;
-                                    @endphp
-
-                                    @if($otherProductsCount > 0)
-                                        (and {{$otherProductsCount}} other product{{($otherProductsCount > 1) ? 's' : ''}})
+                            @foreach($orders as $order)
+                                <tr>
+                                <td style="width: 18%">
+                                    @if(isset($order->orderDetails) && count($order->orderDetails) > 0)
+                                        @foreach($order->orderDetails as $orderDetail)
+                                            @if(isset($orderDetail->product) && isset($orderDetail->product->productImages) && count($orderDetail->product->productImages) > 0)
+                                                <img src="{{$orderDetail->product->productImages[0]->path}}" alt="..." class="img-thumbnail" style="width: 100px; height: 100px; object-fit: cover">
+                                                @break
+                                            @endif
+                                        @endforeach
                                     @endif
-                                @endif
-                            </td>
-                            <th style="color: #E6B81D;">${{ number_format($order->total, 2, '.', '') }}</th>
+                                </td>
+                                <td>#{{$order->id}}</td>
+                                <td>
+                                    @if(isset($order->orderDetails) && count($order->orderDetails) > 0)
+                                        {{$order->orderDetails[0]->product->name}}
 
+                                        @php
+                                            $totalQuantity = $order->orderDetails->sum('qty');
+                                            $otherProductsCount = $totalQuantity - 1;
+                                        @endphp
 
-                            <td><a href="/account/my-order/{{$order->id}}" class="btn btn-primary"><i class="fa fa-info-circle"></i></a></td>
-                        </tr>
+                                        @if($otherProductsCount > 0)
+                                            (and {{$otherProductsCount}} other product{{($otherProductsCount > 1) ? 's' : ''}})
+                                        @endif
+                                    @endif
+                                </td>
+                                <th style="color: #E6B81D;">${{ number_format($order->total, 2, '.', '') }}</th>
+                                    <td>{{$order->payment_method}}</td>
+                                    <td>
+                                        @switch($order->status)
+                                            @case(0)<span class="text text-danger">Cancel</span>@break
+                                            @case(1)<span class="text text-secondary">Receive Orders</span>@break
+                                            @case(2)<span class="text text-warning">Unconfirmed</span>@break
+                                            @case(3)<span class="text text-success">Confirmed</span>@break
+                                            @case(4)<span class="text text-primary">Paid</span>@break
+                                            @case(5)<span class="text text-primary">Processing</span>@break
+                                            @case(6)<span class="text text-primary">Shipping</span>@break
+                                            @case(7)<span class="text text-success">Finish</span>@break
+                                        @endswitch
+                                    </td>
+                                <td><a href="/account/my-order/{{$order->id}}" class="btn btn-primary"><i class="fa fa-info-circle"></i></a></td>
+                            </tr>
+                            @endforeach
                         </tbody>
                     </table>
-                @endforeach
                 @else
                     <div class="row justify-content-center">
                         <div class="col-md-6">
