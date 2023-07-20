0128bfd74cb2050c5d51ba18687c8ddbe75e115d
Ha-Huu-Hoang <hahuuhoang2004@gmail.com>
2023-07-20 16:16:52 +0700

    updat1

--- routes/web.php
+++ routes/web.php
@@ -129,14 +129,14 @@ Route::prefix('admin')->middleware('CheckAdminLogin')->group(function (){
     });
     //xử lý product
     Route::prefix('product')->group(function (){
-        Route::get('',[\App\Http\Controllers\Admin\ProductController::class,'index']);
-        Route::get('create',[\App\Http\Controllers\Admin\ProductController::class,'create']);
-        Route::post('store',[\App\Http\Controllers\Admin\ProductController::class,'store']);
-        Route::get('edit/{id}',[\App\Http\Controllers\Admin\ProductController::class,'edit'])->name('product.edit');
-        Route::post('edit/update/{id}',[\App\Http\Controllers\Admin\ProductController::class,'update'])->name('product.update');
+        Route::get('',[\App\Http\Controllers\Admin\ProductController::class,'index'])->can('product.view');
+        Route::get('create',[\App\Http\Controllers\Admin\ProductController::class,'create'])->can('product.add');
+        Route::post('store',[\App\Http\Controllers\Admin\ProductController::class,'store'])->can('product.add');
+        Route::get('edit/{id}',[\App\Http\Controllers\Admin\ProductController::class,'edit'])->name('product.edit')->can('product.edit');
+        Route::post('edit/update/{id}',[\App\Http\Controllers\Admin\ProductController::class,'update'])->name('product.update')->can('product.edit');
         Route::get('show/{id}',[\App\Http\Controllers\Admin\ProductController::class,'show'])->name('product.show');
-        Route::post('action',[\App\Http\Controllers\Admin\ProductController::class,'action']);
-        Route::get('delete/{id}',[\App\Http\Controllers\Admin\ProductController::class,'delete'])->name('delete_product');
+        Route::post('action',[\App\Http\Controllers\Admin\ProductController::class,'action'])->can('product.view');
+        Route::get('delete/{id}',[\App\Http\Controllers\Admin\ProductController::class,'delete'])->name('delete_product')->can('product.delete');
         //xử lý ảnh product
 
     });
@@ -152,20 +152,20 @@ Route::prefix('admin')->middleware('CheckAdminLogin')->group(function (){
     });
 //xử lý route user
     Route::prefix('user')->group(function (){
-        Route::get('',[\App\Http\Controllers\Admin\UsersController::class,'index']);
+        Route::get('',[\App\Http\Controllers\Admin\UsersController::class,'index'])->can('user.view');
         Route::get('show/{id}',[\App\Http\Controllers\Admin\UsersController::class,'show'])->name('user.show');
-        Route::get('create',[\App\Http\Controllers\Admin\UsersController::class,'create']);
-        Route::post('store',[\App\Http\Controllers\Admin\UsersController::class,'store']);
-        Route::post('action',[\App\Http\Controllers\Admin\UsersController::class,'action']);
-        Route::get('edit/{id}',[\App\Http\Controllers\Admin\UsersController::class,'edit'])->name('user.edit');
-        Route::post('edit/update/{id}',[\App\Http\Controllers\Admin\UsersController::class,'update'])->name('user.update');
-        Route::get('delete/{id}',[\App\Http\Controllers\Admin\UsersController::class,'delete'])->name('delete_user');
+        Route::get('create',[\App\Http\Controllers\Admin\UsersController::class,'create'])->can('user.add');
+        Route::post('store',[\App\Http\Controllers\Admin\UsersController::class,'store'])->can('user.add');
+        Route::post('action',[\App\Http\Controllers\Admin\UsersController::class,'action'])->can('user.view');
+        Route::get('edit/{id}',[\App\Http\Controllers\Admin\UsersController::class,'edit'])->name('user.edit')->can('user.edit');
+        Route::post('edit/update/{id}',[\App\Http\Controllers\Admin\UsersController::class,'update'])->name('user.update')->can('user.edit');
+        Route::get('delete/{id}',[\App\Http\Controllers\Admin\UsersController::class,'delete'])->name('delete_user')->can('user.delete');
     });
 
     Route::get('dashboard',[\App\Http\Controllers\Admin\DashboardController::class,'index']);
     Route::get('/statistical',[\App\Http\Controllers\Admin\DashboardController::class,'statistical']);
     Route::get('/order7Days',[\App\Http\Controllers\Admin\DashboardController::class,'order7Days']);
-    Route::get('orders',[\App\Http\Controllers\Admin\OrdersController::class,'index']);
+    Route::get('orders',[\App\Http\Controllers\Admin\OrdersController::class,'index'])->can('orders.view');
     Route::post('/confirm-payment', [\App\Http\Controllers\Admin\OrdersController::class, 'confirmPayment'])->name('confirm.payment');
     Route::post('/orders/{orderId}/cancel', [\App\Http\Controllers\Admin\OrdersController::class,'cancelOrder']);
 
